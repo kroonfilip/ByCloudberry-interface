adcd43e2f0feda70fedab90f90a6a587aa1b7a80
SebastianH-ctrl <61822376+SebastianH-ctrl@users.noreply.github.com>
2022-12-12 13:23:30 +0100

    Update settings.js

--- src/components/settings.js
+++ src/components/settings.js
@@ -3,14 +3,14 @@ import Header from './Header';
 
 function Settings () {
   const [APIStatus, setAPIStatus] = useState("");
-    
+  
   function handleServer() {
     if (APIStatus != "not_suspended") {
     const options = {
       method: 'POST',
       headers: {
         accept: 'application/json',
-        authorization: process.env.RENDER_API_KEY
+        authorization: process.env.REACT_APP_RENDER_API_KEY
       }
     };
     
@@ -24,7 +24,7 @@ function Settings () {
         method: 'POST',
         headers: {
           accept: 'application/json',
-          authorization: process.env.RENDER_API_KEY
+          authorization: process.env.REACT_APP_RENDER_API_KEY
         }
       };
       
@@ -36,12 +36,12 @@ function Settings () {
   }
 
     useEffect(() => {
-
+      
     const options = {
         method: 'GET',
         headers: {
           accept: 'application/json',
-          authorization: process.env.RENDER_API_KEY
+          authorization: process.env.REACT_APP_RENDER_API_KEY
 
         }
       };  
